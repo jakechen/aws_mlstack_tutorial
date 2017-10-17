e816bb6845a7adc8c91c9c1f42b19289f618c1a0
Jake Chen <jakejchen@gmail.com>
2017-10-17 16:40:00 -0500

    lambda pulls local dataset

--- crispdm6-prediction/lambda_function.py
+++ crispdm6-prediction/lambda_function.py
@@ -1,9 +1,12 @@
-batch_size = 1
+batch_size = 5
 
 import mxnet as mx
 import boto3
 
+
 s3 = boto3.client('s3')
+
+# Download saved model
 s3.download_file(
     'jakechenawstemp',
     'mnist_symbol.mxnet',
@@ -15,14 +18,38 @@ s3.download_file(
     './mnist_module.mxnet'
 )
 
-def lambda_handler(event, context)
-	X = mx.io.NDArrayIter(mnist['test_data'], mnist['test_label'], batch_size)
+# Download latest 5 records
+s3.download_file(
+    'jakechenawstemp',
+    'x_test.csv',
+    './x_test.csv'
+)
+s3.download_file(
+    'jakechenawstemp',
+    'y_test.csv',
+    './y_test.csv'
+)
 
-	lenet2 = mx.symbol.load('./mnist_symbol.mxnet')
-	lenet_model2 = mx.mod.Module(lenet2, context=mx.gpu())
-	lenet_model2.bind(X.provide_data, X.provide_label, for_training=False)
-	lenet_model2.load_params('./mnist_module.mxnet')
-	
-	acc = mx.metric.Accuracy()
-	lenet_model2.score(X, acc)
-	print(acc)
+# Load and run predictions using saved model
+def lambda_handler(event, context)
+    test_iter = mx.io.CSVIter(
+        data_csv = './x_test.csv',
+        data_shape = (5, 1, 28, 28),
+        label_csv= './y_test.csv',
+        label_shape = (5,),
+        batch_size = batch_size)
+
+    lenet2 = mx.symbol.load('./mnist_symbol.mxnet')
+    lenet_model2 = mx.mod.Module(lenet2, context=mx.cpu())
+    lenet_model2.bind(test_iter.provide_data, test_iter.provide_label, for_training=False)
+    lenet_model2.load_params('./mnist_module.mxnet')
+
+    acc = mx.metric.Accuracy()
+    lenet_model2.score(test_iter, acc)
+
+    return acc
+
+# For local testing
+if __name__ == '__main__':
+    acc = lambda_handler('','')
+    print(acc)
